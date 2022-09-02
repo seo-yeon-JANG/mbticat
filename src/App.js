8a2de1c41f14f790b52ec6c1c88790788da3fb5c
seoyeon <tjsus247@naver.com>
2022-09-02 12:54:06 +0900

    add fit

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<Home/>} />
-      <Route path='/Question' element={<Question/>} />
-      <Route path='/Result' element={<Result/>} />
-      </Routes>
+      <Route path='/question' element={<Question/>} />
+      <Route path='/result' element={<Result/>} />
+      </Routes> 
   );
 }
 
